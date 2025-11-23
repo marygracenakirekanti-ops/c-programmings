65859a0ed7afb8a6c37cb99eef33d13ac58c6147
marygracenakirekanti-ops <marygracenakirekanti@gmail.com>
2025-11-23 18:15:25 +0530

    Add files via upload

---
+++ increminant operator.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main( )
+{
+	int X=10,p,q,r,s;
+	p=++X;
+	printf("p=%d",p);
+	q=X--;
+	printf("q=%d",q);
+	r=X++;
+	printf("r=%d",r);
+	s=--X;
+	printf("s=%d",s);
+	return 0;
+}
