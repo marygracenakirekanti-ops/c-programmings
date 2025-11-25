328cad7be861979a8a2b6f91fb8d4c1f9279710f
marygracenakirekanti-ops <marygracenakirekanti@gmail.com>
2025-11-25 18:24:48 +0530

    Add files via upload

---
+++ sum of array values.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main( )
+{
+	int a[100],n,i,sum=0;
+	printf("enter array size");
+	scanf("%d",&n);
+	printf("\n enter array value");
+	for(i=0;i<n;i++);
+	{
+		scanf("%d",a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		sum=sum+a[i];
+	}
+	return 0;
+}
