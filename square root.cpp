b50a0dc0d1c18fe13e5dae0c00c432757f969f4d
marygracenakirekanti-ops <marygracenakirekanti@gmail.com>
2025-12-10 21:57:34 +0530

    Add files via upload

---
+++ square root.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main( )
+{
+	int X,Y;
+	printf("enter any number");
+	scanf("%d",&X);
+	Y=sqrt(X);
+	printf("square root of a given number in=%d",Y);
+	return 0;
+	
+}
